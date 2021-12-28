7129d5706c527fedcc5d2ed023358d195c4aa559
leofracca <leonardofraccaroli1999@gmail.com>
2021-12-28 19:20:07 +0100

    First working release

---
+++ ReFoSl/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using ReFoSl.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReFoSl
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ ReFoSl/UserControls/ButtonAndSlider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ReFoSl
+{
+    public partial class ButtonAndSlider : UserControl
+    {
+        /// <summary>
+        /// The ToolTip text of each button
+        /// </summary>
+        public string ToolTipText
+        {
+            get { return (string)GetValue(ToolTipTextProperty); }
+            set { SetValue(ToolTipTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToolTipTextProperty =
+            DependencyProperty.Register("ToolTipText", typeof(string), typeof(ButtonAndSlider), new UIPropertyMetadata(""));
+
+
+        /// <summary>
+        /// The name of the sound that each button needs to play
+        /// </summary>
+        public string SoundName
+        {
+            get { return (string)GetValue(SoundNameProperty); }
+            set { SetValue(SoundNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty SoundNameProperty =
+            DependencyProperty.Register("SoundName", typeof(string), typeof(ButtonAndSlider), new UIPropertyMetadata(""));
+
+
+        /// <summary>
+        /// The path of the image of each button
+        /// </summary>
+        public string ImagePath
+        {
+            get { return (string)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register("ImagePath", typeof(string), typeof(ButtonAndSlider), new UIPropertyMetadata(""));
+
+
+        public ButtonAndSlider()
+        {
+            InitializeComponent();
+        }
+    }
+}
