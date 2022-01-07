876ca314f41be3176c3f9f813a795ab07892bb65
leofracca <leonardofraccaroli1999@gmail.com>
2022-01-07 20:11:35 +0100

    Bug fixes, modified some sounds and optimized sounds looping

---
+++ ReFoSl/Models/SoundEffectInstanceExtended.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework.Audio;
+
+namespace ReFoSl.Models
+{
+    class SoundEffectInstanceExtended
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private SoundEffectInstance _soundEffectInstanceProperty;
+        public SoundEffectInstance SoundEffectInstanceProperty
+        {
+            get { return _soundEffectInstanceProperty; }
+            set { _soundEffectInstanceProperty = value; }
+        }
+
+        private double _volume;
+        public double Volume
+        {
+            get { return _volume; }
+            set { _volume = value; _soundEffectInstanceProperty.Volume = (float)value; }
+        }
+
+
+        public SoundEffectInstanceExtended(string name, SoundEffectInstance soundEffectInstance)
+        {
+            _name = name;
+            _soundEffectInstanceProperty = soundEffectInstance;
+        }
+
+        public void Play(double volume = 0.5f)
+        {
+            Volume = volume;
+            _soundEffectInstanceProperty.IsLooped = true;
+            _soundEffectInstanceProperty.Play();
+        }
+
+        public void Stop()
+        {
+            _soundEffectInstanceProperty.Stop();
+        }
+
+    }
+}
--- ReFoSl/ViewModels/ShellViewModel.cs
+++ ReFoSl/ViewModels/ShellViewModel.cs
@@ -2,18 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows;
-using System.Windows.Media.Animation;
+using Microsoft.Xna.Framework.Audio;
+using ReFoSl.Models;
 
 namespace ReFoSl.ViewModels
 {
     public class ShellViewModel : Screen
     {
-        private const string SOUNDS_FOLDER = "sounds/";
+        private const string SOUNDS_FOLDER = "Sounds/";
         private const string WAV_EXTENSION = ".wav";
 
-        private List<MediaElement> _players = new List<MediaElement>();
+        private List<SoundEffectInstanceExtended> _players = new List<SoundEffectInstanceExtended>();
 
         /// <summary>
         /// Play the selected sound
@@ -24,30 +24,9 @@ namespace ReFoSl.ViewModels
         {
             string sound = SOUNDS_FOLDER + soundName + WAV_EXTENSION;
 
-            //Check if the sound has already being played
-            foreach (MediaElement p in _players)
-            {
-                if (p.Source.ToString().Equals(sound))
-                {
-                    // If it is, then play it
-                    p.Play();
-                    p.BeginAnimation(MediaElement.VolumeProperty, new DoubleAnimation(0, volume, TimeSpan.FromSeconds(1)));
-                    return;
-                }
-            }
-
-            // Otherwise this is the first time for the sound to be played
-            // So, instantiate a new MediaElement, play it and add it to the list
-            MediaElement player = new MediaElement();
-            player.LoadedBehavior = MediaState.Manual;
-            player.UnloadedBehavior = MediaState.Manual;
-            player.Source = new Uri(sound, UriKind.Relative);
-
-            player.Play();
-
-            // Fade in animation
-            player.BeginAnimation(MediaElement.VolumeProperty, new DoubleAnimation(0, volume, TimeSpan.FromSeconds(1)));
-            player.MediaEnded += new RoutedEventHandler(MediaElement_MediaEnded);
+            SoundEffectInstanceExtended player = new SoundEffectInstanceExtended(sound,
+                SoundEffect.FromStream(Application.GetContentStream(new Uri(sound, UriKind.Relative)).Stream).CreateInstance());
+            player.Play(volume);
 
             _players.Add(player);
         }
@@ -61,15 +40,12 @@ namespace ReFoSl.ViewModels
             string sound = SOUNDS_FOLDER + soundName + WAV_EXTENSION;
 
             // Loop through the players to find the one that is playing the selected sound
-            foreach (MediaElement player in _players)
+            foreach (SoundEffectInstanceExtended player in _players)
             {
-                if (player.Source.ToString().Equals(sound))
+                if (player.Name.Equals(sound))
                 {
-                    // Fade out animation
-                    DoubleAnimation fadeOut = new DoubleAnimation(player.Volume, 0, TimeSpan.FromSeconds(1));
-                    fadeOut.Completed += (sender, e) => DoubleAnimationFadeOut_Completed(sender, e, player);
-                    player.BeginAnimation(MediaElement.VolumeProperty, fadeOut);
-
+                    player.Stop();
+                    _players.Remove(player);
                     break;
                 }
             }
@@ -84,26 +60,14 @@ namespace ReFoSl.ViewModels
         {
             string sound = SOUNDS_FOLDER + soundName + WAV_EXTENSION;
 
-            foreach (MediaElement player in _players)
+            foreach (SoundEffectInstanceExtended player in _players)
             {
-                if (player.Source.ToString().Equals(sound))
+                if (player.Name.Equals(sound))
                 {
-                    player.BeginAnimation(MediaElement.VolumeProperty, new DoubleAnimation(player.Volume, slider.Value, TimeSpan.FromSeconds(1)));
+                    player.Volume = slider.Value;
                     break;
                 }
             }
         }
-
-        private void MediaElement_MediaEnded(object sender, EventArgs e)
-        {
-            ((MediaElement)sender).Position = TimeSpan.Zero;
-            ((MediaElement)sender).Play();
-        }
-
-        private void DoubleAnimationFadeOut_Completed(object sender, EventArgs e, MediaElement player)
-        {
-            player.Stop();
-        }
-
     }
 }
